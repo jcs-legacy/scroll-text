113b8b3ebe214b928843f868289fc3cd3863b990
JenChieh <jcs090218@gmail.com>
2020-10-05 16:30:44 +0800

    Add core.

---
+++ scroll-text.el
@@ -0,0 +1,38 @@
+;;; scroll-text.el --- Scroll the text for content  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Shen, Jen-Chieh
+;; Created date 2020-10-05 16:19:50
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Scroll the text for content.
+;; Keyword: scroll animation animate text
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/scroll-text
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Scroll the text for content.
+;;
+
+;;; Code:
+
+
+
+(provide 'scroll-text)
+;;; scroll-text.el ends here
