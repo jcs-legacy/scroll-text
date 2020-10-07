e98dbb808f7a11144701ba57a23f5e64affcf1af
Jen-Chieh <jcs090218@gmail.com>
2020-10-07 15:04:29 +0800

    Bump version.

--- scroll-text.el
+++ scroll-text.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Scroll the text for content.
 ;; Keyword: scroll animation animate text
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "24.3") (dash "2.14.1"))
 ;; URL: https://github.com/jcs-elpa/scroll-text
 
